89f334b33ee5d132777927e5b634944c4f1b7e91
YuanWu <hjf6318567@gmail.com>
2020-09-20 17:23:53 -0400

    Update train.py

--- train.py
+++ train.py
@@ -67,43 +67,43 @@ class LeNet_Classifier(nn.Module):
 
 
 class AdversarialNetwork(nn.Module):
-  def __init__(self, in_feature, hidden_size):
-    super(AdversarialNetwork, self).__init__()
-    self.ad_layer1 = nn.Linear(in_feature, hidden_size)
-    self.ad_layer2 = nn.Linear(hidden_size, hidden_size)
-    self.ad_layer3 = nn.Linear(hidden_size, 1)
-    self.relu1 = nn.ReLU()
-    self.relu2 = nn.ReLU()
-    self.dropout1 = nn.Dropout(0.5)
-    self.dropout2 = nn.Dropout(0.5)
-    self.sigmoid = nn.Sigmoid()
-    self.apply(init_weights)
-    self.iter_num = 0
-    self.alpha = 10
-    self.low = 0.0
-    self.high = 1.0
-    self.max_iter = 10000.0
+    def __init__(self, in_feature, hidden_size):
+        super(AdversarialNetwork, self).__init__()
+        self.ad_layer1 = nn.Linear(in_feature, hidden_size)
+        self.ad_layer2 = nn.Linear(hidden_size, hidden_size)
+        self.ad_layer3 = nn.Linear(hidden_size, 1)
+        self.relu1 = nn.ReLU()
+        self.relu2 = nn.ReLU()
+        self.dropout1 = nn.Dropout(0.5)
+        self.dropout2 = nn.Dropout(0.5)
+        self.sigmoid = nn.Sigmoid()
+        self.apply(init_weights)
+        self.iter_num = 0
+        self.alpha = 10
+        self.low = 0.0
+        self.high = 1.0
+        self.max_iter = 10000.0
 
-  def forward(self, x):
-    if self.training:
-        self.iter_num += 1
-    coeff = calc_coeff(self.iter_num, self.high, self.low, self.alpha, self.max_iter)
-    x = x * 1.0
-    x.register_hook(grl_hook(coeff))
-    x = self.ad_layer1(x)
-    x = self.relu1(x)
-    x = self.dropout1(x)
-    x = self.ad_layer2(x)
-    x = self.relu2(x)
-    x = self.dropout2(x)
-    y = self.ad_layer3(x)
-    y = self.sigmoid(y)
-    return y
+    def forward(self, x):
+        if self.training:
+            self.iter_num += 1
+        coeff = calc_coeff(self.iter_num, self.high, self.low, self.alpha, self.max_iter)
+        x = x * 1.0
+        x.register_hook(grl_hook(coeff))
+        x = self.ad_layer1(x)
+        x = self.relu1(x)
+        x = self.dropout1(x)
+        x = self.ad_layer2(x)
+        x = self.relu2(x)
+        x = self.dropout2(x)
+        y = self.ad_layer3(x)
+        y = self.sigmoid(y)
+        return y
 
-  def output_num(self):
-    return 1
-  def get_parameters(self):
-    return [{"params":self.parameters(), "lr_mult":10, 'decay_mult':2}]
+    def output_num(self):
+        return 1
+    def get_parameters(self):
+        return [{"params":self.parameters(), "lr_mult":10, 'decay_mult':2}]
 
 
 def mixup_data(x, y, alpha = 0.2):
@@ -233,14 +233,14 @@ def main():
           
 
     if args.task == 'U2M':
-        source_list = '/home/yuanwu3/projects/def-inkpen/yuanwu3/Project4/data/usps_train.txt'
-        target_list = '/home/yuanwu3/projects/def-inkpen/yuanwu3/Project4/data/mnist_train.txt'
-        test_list = '/home/yuanwu3/projects/def-inkpen/yuanwu3/Project4/data/mnist_test.txt'
+        source_list = '/data/usps_train.txt'
+        target_list = '/data/mnist_train.txt'
+        test_list = '/data/mnist_test.txt'
         start_epoch = 1
     elif args.task == 'M2U':
-        source_list = '/home/yuanwu3/projects/def-inkpen/yuanwu3/Project4/data/mnist_train.txt'
-        target_list = '/home/yuanwu3/projects/def-inkpen/yuanwu3/Project4/data/usps_train.txt'
-        test_list = '/home/yuanwu3/projects/def-inkpen/yuanwu3/Project4/data/usps_test.txt'
+        source_list = '/data/mnist_train.txt'
+        target_list = '/data/usps_train.txt'
+        test_list = '/data/usps_test.txt'
         start_epoch = 1
     else:
         raise Exception('task cannot be recognized!')
